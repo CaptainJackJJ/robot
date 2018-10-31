c6285ef905aad6793be8fd69567bec82718a91cd
jun jiang <CaptainJackJJ@outlook.com>
2018-10-31 12:37:04 +0800

    Add supervisor

---
+++ supervisor/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace supervisor
+{
+    partial class FormSupervisor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerSupervisor = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timerSupervisor
+            // 
+            this.timerSupervisor.Enabled = true;
+            this.timerSupervisor.Interval = 60000;
+            this.timerSupervisor.Tick += new System.EventHandler(this.timerSupervisor_Tick);
+            // 
+            // FormSupervisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "FormSupervisor";
+            this.Text = "监工";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerSupervisor;
+    }
+}
+
---
+++ supervisor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace supervisor
+{
+    public partial class FormSupervisor : Form
+    {
+        public FormSupervisor()
+        {
+            InitializeComponent();
+        }
+
+        private void timerSupervisor_Tick(object sender, EventArgs e)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName("experiment").ToList().Count <= 0)
+            {
+                System.Diagnostics.Process.Start("experiment.exe");
+            }
+        }
+    }
+}
