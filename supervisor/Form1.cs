620653d62eb52762fc73d9cacf55ac39345b2a96
jun jiang <CaptainJackJJ@outlook.com>
2018-11-06 11:27:31 +0800

    [supervisor] wait IE cache clear done

--- supervisor/Form1.cs
+++ supervisor/Form1.cs
@@ -20,12 +20,19 @@ namespace supervisor
         }
 
         private void timerSupervisor_Tick(object sender, EventArgs e)
-        {            
-            if (System.Diagnostics.Process.GetProcessesByName("experiment").ToList().Count <= 0)
+        {
+            try
             {
-                CleanTempFiles();
+                if (System.Diagnostics.Process.GetProcessesByName("experiment").ToList().Count <= 0)
+                {
+                    CleanTempFiles();
 
-                System.Diagnostics.Process.Start("experiment.exe");
+                    System.Diagnostics.Process.Start("experiment.exe");
+                }
+            }
+            catch
+            {
+
             }
         }
 
@@ -62,7 +69,8 @@ namespace supervisor
             p.FileName = "cmd.exe";
             p.Arguments = "/c " + cmd;
             p.WindowStyle = ProcessWindowStyle.Hidden;  // Use a hidden window
-            Process.Start(p);
+            Process pro = Process.Start(p);
+            pro.WaitForExit();
         }
         /// <summary>
         /// 删除临时文件
