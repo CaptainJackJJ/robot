02b51a18270f36a7de1ac99c5764b4175207728a
jun jiang <CaptainJackJJ@outlook.com>
2018-11-09 22:49:47 +0800

    Add csdnAccountDb

---
+++ CsdnAcountDb/DbCsdnAccountDb.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SQLite;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace CsdnAcountDb
+{
+    class DbCsdnAccountDb
+    {
+        public class AccountInfo
+        {
+            public long id;
+            public string csdnUsername;
+            public string csdnPassword;
+            public string email;
+            public string emailPassword;
+            public string emailServer;
+        }
+
+        private string connStr = @"data source=";
+
+        public DbCsdnAccountDb(string dbName)
+        {
+            connStr += dbName;
+        }
+
+        // 执行增加、删除、修改指令
+        public int ExecuteNonQuery(string sql/*, params OleDbParameter[] param*/)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(connStr))
+                {
+                    conn.Open();
+                    using (SQLiteCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+
+                        int res = cmd.ExecuteNonQuery();
+                        conn.Close();
+                        conn.Dispose();
+
+                        return res;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.WriteLog(LogType.Error, "sql: " + sql + ", e msg" + e.Message);
+            }
+            return -1;
+        }
+
+
+        // 执行查询指令，获取返回的datareader
+        public SQLiteDataReader ExecuteReader(string sql/*, params OleDbParameter[] param*/)
+        {
+            try
+            {
+                SQLiteConnection conn = new SQLiteConnection(connStr);
+                conn.Open();
+
+                SQLiteCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sql;
+
+                SQLiteDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                return reader;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+
+
+        public bool AddAccountInfo(AccountInfo info)
+        {
+            string sql = "INSERT INTO account ( csdnUsername, csdnPassword, email, emailServer )"
+            + " VALUES ('" + info.csdnUsername + "','" + info.csdnPassword + "','" + info.email + "','" + info.emailServer + "')";
+
+            if (ExecuteNonQuery(sql) <= 0)
+            {
+                Log.WriteLog(LogType.Error, "sql: " + sql );
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CsdnAcountDb/FormCsdnAccountDb.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CsdnAcountDb
+{
+    public partial class FormCsdnAccountDb : Form
+    {
+        public FormCsdnAccountDb()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCsdnAccountDb_Load(object sender, EventArgs e)
+        {
+            DbCsdnAccountDb accountDb = new DbCsdnAccountDb("CsdnAccount.db");
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText("www.csdn.net.sql"))
+            {
+                string s;
+                int indexFlag1,indexFlag2,indexAt;
+                DbCsdnAccountDb.AccountInfo info = new DbCsdnAccountDb.AccountInfo();
+                while ((s = sr.ReadLine()) != null)
+                {
+                    indexFlag1 = s.IndexOf("#");
+                    indexFlag2 = s.LastIndexOf("#");
+
+                    info.csdnUsername = s.Substring(0, indexFlag1 - 1);
+                    info.csdnPassword = s.Substring(indexFlag1 + 2, indexFlag2 - indexFlag1 - 3);
+                    info.email = s.Substring(indexFlag2 + 2, s.Length - indexFlag2 - 2);
+
+                    indexAt = info.email.IndexOf("@");
+                    info.emailServer = info.email.Substring(indexAt + 1, info.email.Length - indexAt - 1);
+
+                    accountDb.AddAccountInfo(info);
+                }
+            }
+        }
+    }
+}
