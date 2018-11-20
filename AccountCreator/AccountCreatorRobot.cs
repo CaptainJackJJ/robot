f8e3fe455ead9ba7c0349620ec8a4620121cd008
jun jiang <CaptainJackJJ@outlook.com>
2018-11-20 15:05:49 +0800

    [creator] play sound

--- AccountCreator/AccountCreatorRobot.cs
+++ AccountCreator/AccountCreatorRobot.cs
@@ -216,6 +216,7 @@ namespace AccountCreator
             m_browser.SafeNavigate("https://i.csdn.net/#/uc/profile");
             
             m_timerBrain.Enabled = false;
+            System.Media.SystemSounds.Beep.Play();
         }
 
         private void ConfirmLoginWithAccount()
