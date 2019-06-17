b4cb05107eb3149573c290a5df0c34f6c8130731
jun jiang <CaptainJackJJ@outlook.com>
2019-06-17 21:55:17 +0800

    [Creator] clear ie cache to make other account fake login

--- AccountCreator/FormAccountCreator.cs
+++ AccountCreator/FormAccountCreator.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AccountCreator
 {
@@ -16,6 +17,8 @@ namespace AccountCreator
 
         public FormAccountCreator()
         {
+            CleanTempFiles();
+
             InitializeComponent();
 
             Tools.SetWebBrowserFeatures(11);
@@ -25,6 +28,54 @@ namespace AccountCreator
             m_Robot = new AccountCreatorRobot(webBrowser1, timer1);
 
             CreateAccount();
+        }
+
+        /// <summary>
+        /// 清除文件夹
+        /// </summary>
+        /// <param name="path">文件夹路径</param>
+        static void FolderClear(string path)
+        {
+            try
+            {
+                System.IO.DirectoryInfo diPath = new System.IO.DirectoryInfo(path);
+                foreach (System.IO.FileInfo fiCurrFile in diPath.GetFiles())
+                {
+                    fiCurrFile.Delete();
+                    //FileDelete(fiCurrFile.FullName);
+
+                }
+                foreach (System.IO.DirectoryInfo diSubFolder in diPath.GetDirectories())
+                {
+                    FolderClear(diSubFolder.FullName); // Call recursively for all subfolders
+                }
+            }
+            catch
+            { }
+        }
+        /// <summary>
+        /// 执行命令行
+        /// </summary>
+        /// <param name="cmd"></param>
+        static void RunCmd(string cmd)
+        {
+            ProcessStartInfo p = new ProcessStartInfo();
+            p.FileName = "cmd.exe";
+            p.Arguments = "/c " + cmd;
+            p.WindowStyle = ProcessWindowStyle.Hidden;  // Use a hidden window
+            Process pro = Process.Start(p);
+            pro.WaitForExit();
+        }
+        /// <summary>
+        /// 删除临时文件
+        /// </summary>
+        public static void CleanTempFiles()
+        {
+            FolderClear(Environment.GetFolderPath(Environment.SpecialFolder.InternetCache));
+
+            FolderClear(Environment.GetFolderPath(Environment.SpecialFolder.Cookies));
+
+            RunCmd("RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 22783");
         }
 
         private void FormAccountCreator_Load(object sender, EventArgs e)
