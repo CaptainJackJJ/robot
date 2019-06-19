eee84004c4005e581034511e4aea713de98e4f21
jun jiang <CaptainJackJJ@outlook.com>
2019-06-20 07:47:07 +0800

    5 seconds one step

--- experiment/BlogRobot.cs
+++ experiment/BlogRobot.cs
@@ -70,7 +70,7 @@ namespace experiment
 #if DEBUG
             m_timerBrain.Interval = 2000;
 #else
-            m_timerBrain.Interval = 4000;
+            m_timerBrain.Interval = 5000;
 #endif
         }
 
