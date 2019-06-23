8d1edee0d3667a79f727932b1d559af7bc383ecd
jun jiang <CaptainJackJJ@outlook.com>
2019-06-23 21:52:05 +0800

    [juejin] add juejin pro

---
+++ JuejinRobot/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+using System.Net.Security;
+
+namespace experiment
+{
+    public partial class Form1 : Form
+    {
+        BlogRobot m_blogRobot = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Tools.SetWebBrowserFeatures(11);
+            this.Text = this.Text + "_IE" + Tools.GetBrowserVersion().ToString();
+
+            m_blogRobot = new BlogRobot(webBrowser1, timerRobotBrain);
+        }
+
+        private void navigateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://blog.csdn.net/jiangjunshow/article/details/77711593");
+        }
+
+        private void submitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void accountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void timerRobotBrain_Tick(object sender, EventArgs e)
+        {
+            m_blogRobot.timerBrain();
+        }
+    }
+}
